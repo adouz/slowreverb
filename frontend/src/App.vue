6531e53828228a513c82fbab2f112ff138009011
adouz <abdllatifusk@gmail.com>
2020-10-21 01:44:51 +0100

    more opts

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,18 +1,31 @@
 <template>
   <div id="app">
-    <input type="file" @change="handlefile">
-    <input type="submit" value="play" @click="play">
-    <input type="submit" value="stop" @click="stop">
     <p>
-      pitch rate
-      <input type="number" min="0.1" max="1" step="0.1" v-model="rate">
-      {{rate}}
+      playback rate
+      <input type="number" min="0" max="1" step="0.1" v-model="playback">
     </p>
     <p>
       reverb wet
-      <input type="number" min="0.1" max="1" step="0.1" v-model="wet">
-      {{wet}}
+      <input type="number" min="0" max="1" step="0.1" v-model="wet">
     </p>
+    <p>
+      reverb decay
+      <input type="number" min="1" max="5" step="1" v-model="decay">
+    </p>
+    <p>
+      reverb preDelay
+      <input type="number" min="0" max="1" step="0.01" v-model="preDelay">
+    </p>
+    <p>
+      detune
+      <input type="number" min="0" max="100" step="10" v-model="detune">
+    </p>
+    <p>
+      <input type="submit" value="start" @click="start">
+    </p>
+    <input type="file" @change="start">
+    <input type="submit" value="play" @click="play">
+    <input type="submit" value="stop" @click="stop">
   </div>
 </template>
 
@@ -25,20 +38,27 @@ export default {
   data (){
     return {
       buffer: null,
-      rate: 0.8,
       fileurl: null,
       player: null,
-      wet: 1,
       offset:0,
-      startedAt:0
+      startedAt:0,
+      playback: 0.8,
+      wet: 1,
+      decay: 3,
+      preDelay: 0.0,
+      detune: 10
     }
   },
   mounted: function (){
   },
   methods:{
-    handlefile(e){
-      const file = e.target.files[0];
-      this.fileurl =  URL.createObjectURL(file);
+    start(e){
+      console.log(e)
+      if (e.target.files){
+        const file = e.target.files[0];
+        this.fileurl =  URL.createObjectURL(file);
+      }
+      if (this.player && this.player.state === "started") this.stop();
       this.offset = 0;
       this.startedAt = 0;
       this.play();
@@ -48,13 +68,15 @@ export default {
       this.player.stop();
       console.log(Tone.now());
     },
-    async play(){
+    play(){
       const reverb = new Tone.Reverb({
-        decay: 2.5,
-        wet: this.wet
+        decay: this.decay,
+        wet: this.wet,
+        preDelay: this.preDelay
       }).toDestination();
-      this.player = new Tone.Player(this.fileurl).connect(reverb);
-      this.player.playbackRate = this.rate;
+      this.player = new Tone.GrainPlayer(this.fileurl).connect(reverb);
+      this.player.playbackRate = this.playback;
+      this.player.detune = this.detune;
       this.player.loop = true;
       Tone.loaded().then(() => {
         this.startedAt = Tone.now() - this.offset
